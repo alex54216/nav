b37d87ae22f89348003152ecfe6a4202ca34d170
alex54216 <rucvincent@gmail.com>
2020-08-10 19:14:29 +0800

    第一版导航

--- src/main.js
+++ src/main.js
@@ -1,49 +1,17 @@
-const $siteList = $('.siteList')
-const $lastLi = $siteList.find('li.last')
 const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)
 const hashMap = xObject || [
   {logo: 'A', url: 'https://www.acfun.cn'},
   {logo: 'B', url: 'https://www.bilibili.com'}
 ]
-const simplifyUrl = (url) => {
-  return url.replace('https://', '')
-    .replace('http://', '')
-    .replace('www.', '')
-    .replace(/\/.*/, '') // 删除 / 开头的内容
+const simplifyUrl = (url)=>{
+  return url.replace('https://','').replace('http://','').replace('www.','').replace(/\/.*/,'')
 }
-
-const render = () => {
-  $siteList.find('li:not(.last)').remove()
-  hashMap.forEach((node, index) => {
-    const $li = $(`<li>
-      <div class="site">
-        <div class="logo">${node.logo}</div>
-        <div class="link">${simplifyUrl(node.url)}</div>
-        <div class="close">
-          <svg class="icon">
-            <use xlink:href="#icon-close"></use>
-          </svg>
-        </div>
-      </div>
-    </li>`).insertBefore($lastLi)
-    $li.on('click', () => {
-      window.open(node.url)
-    })
-    $li.on('click', '.close', (e) => {
-      e.stopPropagation() // 阻止冒泡
-      hashMap.splice(index, 1)
-      render()
-    })
-  })
-}
-
-render()
-
-$('.addButton').on('click', () => {
-  let url = window.prompt('请问你要添加的网址是啥？')
-  if (url.indexOf('http') !== 0) {
-    url = 'https://' + url
+// 添加网址
+$('.last').on('click',()=>{
+  let url = window.prompt('请输入您添加的网址')
+  if(url.indexOf('http') !== 0){
+    url = 'https://'+url
   }
   console.log(url)
   hashMap.push({
@@ -52,7 +20,32 @@ $('.addButton').on('click', () => {
   })
   render()
 })
-
+// render函数
+const render = ()=>{
+  $('.siteList').find('li:not(.last)').remove()
+  hashMap.forEach((item,index)=>{
+    const $li = $(`<li>
+      <div class="site">
+        <div class="logo">${item.logo}</div>
+        <div class="link">${simplifyUrl(item.url)}</div>
+        <div class="close">
+          <svg class="icon">
+            <use xlink:href="#icon-close"></use>
+          </svg>
+        </div>
+      </div>
+    </li>`).insertBefore($('.last'))
+    $li.on('click', () => {
+      window.open(item.url)
+    })
+    $li.on('click', '.close', (e) => {
+      e.stopPropagation() // 阻止冒泡
+      hashMap.splice(index, 1)
+      render()
+    })
+  })
+}
+render()
 window.onbeforeunload = () => {
   const string = JSON.stringify(hashMap)
   localStorage.setItem('x', string)
